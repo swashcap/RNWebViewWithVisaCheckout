0bfbd541dd93ee168dc88c451a63a6026bb81dcc
Cory Reed <swashcap@gmail.com>
2018-10-22 12:10:39 -0700

    feat(server): add basic file server setup

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+// @flow
+const Koa = require('koa')
+const logger = require('koa-logger')
+const render = require('koa-ejs')
+const path = require('path')
+
+const app = new Koa();
+const port = process.env.PORT || 4000
+const VISA_CHECKOUT_API_KEY = process.env.API_KEY;
+
+app.use(logger())
+render(app, {
+  root: path.join(__dirname, 'view'),
+  layout: false,
+  cache: process.env.NODE_ENV !== 'development',
+  debug: process.env.NODE_ENV === 'development'
+})
+
+app.use(async (ctx) => {
+  await ctx.render('index', { VISA_CHECKOUT_API_KEY });
+})
+
+if (require.main === module) {
+  app.listen(port) 
+  console.log(`Listening on port ${port}`)
+}
+
+module.exports = app;
+
