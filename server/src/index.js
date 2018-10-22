3499045213fd97e3d9d664e7d905680f2c7bd781
Cory Reed <swashcap@gmail.com>
2018-10-22 15:35:09 -0700

    feat(server): wire up static public asset serving

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const Koa = require('koa')
 const logger = require('koa-logger')
 const render = require('koa-ejs')
+const koaStatic = require('koa-static')
+const {get} = require('koa-route');
 const path = require('path')
 
 const app = new Koa();
@@ -11,15 +13,16 @@ const port = process.env.PORT || 4000
 const VISA_CHECKOUT_API_KEY = process.env.VISA_CHECKOUT_API_KEY;
 
 app.use(logger())
+app.use(koaStatic(path.resolve(__dirname, '../public')));
 render(app, {
   root: path.join(__dirname, 'view'),
   layout: false,
   cache: process.env.NODE_ENV !== 'development'
 })
 
-app.use(async (ctx) => {
+app.use(get('/', async (ctx) => {
   await ctx.render('index', { VISA_CHECKOUT_API_KEY });
-})
+}))
 
 if (require.main === module) {
   app.listen(port) 
